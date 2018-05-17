d12fb66f793d2458a8822967b44a6466de980ade
Dan <mail@dannilsson.se>
2018-05-17 20:20:34 +0200

    add exception

--- app/lib/danNil76/Rest/Client.php
+++ app/lib/danNil76/Rest/Client.php
@@ -5,11 +5,17 @@ namespace danNil76\Rest;
 use GuzzleHttp\Client as GClient;
 use GuzzleHttp\Command\Guzzle\Description as GDescription;
 use GuzzleHttp\Command\Guzzle\GuzzleClient as GGuzzClient;
+use GuzzleHttp\Command\Exception\CommandClientException;
 
 class Client extends GGuzzClient
 {
 	public static function create( $config = [] )
 	{
+		if( !is_file($config['service']) )
+		{
+			throw new \Exception('Service file not found! Looking for: ' . $config['service'], 1);
+		}
+
 		$serviceJson = json_decode( file_get_contents( $config['service'] ), true );
 
 		$serviceDesc = new GDescription(
